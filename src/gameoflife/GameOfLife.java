2b3128d2a1ff6ea5d11bb72f6acd007ac8686c2f
Justin Beringer <jbberinger@hotmail.com>
2018-05-09 01:59:55 -0700

    Code optimized and cleaned up. Buttons added to start, pause, and clear cells. "Nuke" button also added to randomly assign living cells to the grid.
    - UI bug upon running program shifts buttons to the right by a few
    pixels.

--- src/gameoflife/GameOfLife.java
+++ src/gameoflife/GameOfLife.java
@@ -13,172 +13,268 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import java.util.concurrent.ThreadLocalRandom;
-import javafx.scene.layout.StackPane;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 /**
  *
  * @author Justin Beringer
+ * 
+ * JavaFX Implementation of Conway's Game of Life
+ * Inspired by: https://www.youtube.com/watch?v=FWSR_7kZuYg&t=855s
+ * 
  */
 public class GameOfLife extends Application {
 
-    final int WIDTH = 500; // window width
-    final int HEIGHT = 500; // window height
-    final int SCALE = 5; // grid size
+    final int WIDTH = 1000; // grid width
+    final int HEIGHT = 700; // grid height
+    final int SCALE = 20; // cell scale factor
     final int COLUMNS = WIDTH / SCALE;
     final int ROWS = HEIGHT / SCALE;
 
-    // maps cells to position on board
-    Map<String, Cell> boardMap = new HashMap<>();
-    // allows boardMap to be iterated non-destructively
+    // maps cells to position on grid
+    Map<Position, Cell> gridMap = new HashMap<>();
+
+    // allows gridMap to be iterated non-destructively
     boolean[][] lifeGrid = new boolean[COLUMNS][ROWS];
 
-    // tracks the number of generations
+    // tracks the number of generations aka 'ticks'
     int generation = 0;
 
-    StackPane table;
-    Pane board;
-    Text text;
+    VBox vBox;
+    Pane grid;
+    HBox controls;
+    Button startButton;
+    Button pauseButton;
+    Button clearButton;
+    Button nukeButton;
+    Text generationCounter;
 
     @Override
     public void start(Stage primaryStage) {
 
         // Initializes timeline
-        final Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.1), new EventHandler() {
+        @SuppressWarnings("Convert2Lambda")
+        final Timeline timeline = new Timeline(new KeyFrame(Duration.millis(32), new EventHandler() {
             @Override
             public void handle(Event event) {
-                updateBoard();
+                updateGrid();
             }
         }));
 
-        // Timeline runs indefinitely
-        timeline.setCycleCount(Timeline.INDEFINITE);
+        grid = new Pane();
+        seed();
+        vBox = new VBox();
+        vBox.setPrefWidth(WIDTH);
+        vBox.getChildren().add(grid);
+        vBox.setStyle("-fx-background-color: black;");
+        controls = new HBox();
+        
+        controls.setPrefWidth(WIDTH);
+        controls.setPrefWidth(WIDTH);
+        controls.setPrefWidth(WIDTH);
+        
+        controls.setStyle("-fx-background-color: rgb(20, 20, 20);");
+        controls.setPrefHeight(50);
+        controls.setSpacing(100);
+        controls.setAlignment(Pos.CENTER);
+        
+        startButton = new Button("start");
+        pauseButton = new Button("pause");
+        clearButton = new Button("clear");
+        nukeButton = new Button("☢ nuke ☢");
 
-        table = new StackPane();
-        board = new Pane();
+        initializeButtonListeners(timeline);
 
-        text = new Text(Integer.toString(generation));
-        text.setFont(new Font(100));
-        text.setFill(Color.rgb(255, 255, 0, 0.75));
+        controls.getChildren().add(startButton);
+        controls.getChildren().add(pauseButton);
+        controls.getChildren().add(clearButton);
+        controls.getChildren().add(nukeButton);
+        
+        generationCounter = new Text("generation: " + Integer.toString(generation));
+        generationCounter.setFont(new Font(15));
+        generationCounter.setFill(Color.WHITE);
 
-        table.getChildren().add(board);
-        Scene scene = new Scene(table);
+        /**
+         * adds generation counter to controls
+         */
+        //controls.getChildren().add(generationCounter);
+
+        vBox.getChildren().add(controls);
+        Scene scene = new Scene(vBox);
         scene.setFill(Color.BLACK);
 
-        seed();
-
-        //primaryStage.setResizable(false);
         primaryStage.setTitle("Game of Life");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.sizeToScene();
         primaryStage.show();
-
-        //draw();
-        // move this into draw later
-        timeline.play();
     }
 
+    // initializes cells
     private void seed() {
 
-        for (int c = 0; c < COLUMNS; c++) {
-            for (int r = 0; r < ROWS; r++) {
+        for (int row = 0; row < ROWS; row++) {
+            for (int column = 0; column < COLUMNS; column++) {
 
-                String position = c + " " + r;
+                int xPos = column * SCALE;
+                int yPos = row * SCALE;
 
-                int xPos = c * SCALE;
-                int yPos = r * SCALE;
+                Cell cell = new Cell(xPos, yPos, SCALE, SCALE);
 
-                Cell cell = new Cell(xPos, yPos, SCALE - 1, SCALE - 1);
+                cell.setOnMouseClicked((MouseEvent t) -> {
+                    if (t.getButton() == MouseButton.SECONDARY) {
+                        cell.setIsAlive(false);
+                    } else {
+                        cell.setIsAlive(true);
+                    }
+                });
 
-                boolean isAlive;
-                // randomly spawns living cells
-                isAlive = ThreadLocalRandom.current().nextInt(0, 1 + 1) == 1;
+                cell.setOnMouseDragEntered((MouseEvent t) -> {
+                    if (t.getButton() == MouseButton.SECONDARY) {
+                        cell.setIsAlive(false);
+                    } else {
+                        cell.setIsAlive(true);
+                    }
+                });
 
-                cell.setIsAlive(isAlive);
-
-                boardMap.put(position, cell);
-
-                board.getChildren().add(cell);
-
+                Position cellPosition = new Position(column, row);
+                gridMap.put(cellPosition, cell);
+                grid.getChildren().add(cell);
             }
         }
-        generation++;
-
-         //displayes generation counter
-        table.getChildren().remove(text);
-        text.setText(Integer.toString(generation));
-        table.getChildren().add(text);
     }
 
-    int liveNeighbours = 0; // tracks living neighbours
-    String cellPosition;
-    String checkPosition;
+    // track living neighbours
+    private void updateGrid() {
 
-    private void updateBoard() {
+        int liveNeighbours;
+        Position gridPosition = new Position(0, 0);
+
+        gridPosition.clear();
 
         // iterates over each cell
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLUMNS; c++) {
+        for (int row = 0; row < ROWS; row++) {
+            for (int column = 0; column < COLUMNS; column++) {
 
-                liveNeighbours = 0;
-                cellPosition = c + " " + r;
-                Cell bufferCell = boardMap.get(cellPosition);
+                gridPosition.setPosition(column, row);
+
                 // gets initial life state
-                lifeGrid[c][r] = bufferCell.getIsAlive();
+                lifeGrid[column][row] = gridMap.get(gridPosition).getIsAlive();
 
-                // counts living neighbours
-                // index ternary adjusts for edges
-                int neighbourColumn;
-                int neighbourRow;
-                for (int rowBuffer = -1; rowBuffer < 2; rowBuffer++) {
-                    for (int columnBuffer = -1; columnBuffer < 2; columnBuffer++) {
-
-                        neighbourColumn = (c + columnBuffer + COLUMNS) % COLUMNS;
-                        neighbourRow = (r + rowBuffer + ROWS) % ROWS;
-
-                        checkPosition = neighbourColumn + " " + neighbourRow;
-
-                        if (boardMap.get(checkPosition).getIsAlive() && !checkPosition.equals(cellPosition)) {
-                            liveNeighbours++;
-                        }
-                    }
-                }
+                liveNeighbours = checkNeighboursAround(column, row);
 
                 // enforces rules on the cells
-                if (bufferCell.getIsAlive()) {
+                if (gridMap.get(gridPosition).getIsAlive()) {
                     if (liveNeighbours < 2 || liveNeighbours > 3) {
                         // cell dies due to underpopulation OR overpopulation
-                        lifeGrid[c][r] = false;
+                        lifeGrid[column][row] = false;
                     }
                 } else if (liveNeighbours == 3) {
                     // neighbours reproduce
-                    lifeGrid[c][r] = true;
+                    lifeGrid[column][row] = true;
                 }
             }
         }
 
-        for (int i = 0; i < lifeGrid.length; i++) {
-            for (int j = 0; j < lifeGrid.length; j++) {
-                boardMap.get(j + " " + i).setIsAlive(lifeGrid[j][i]);
+        // updates hashmap with cell life states
+        gridPosition.clear();
+        grid.getChildren().clear();
+        for (int row = 0; row < ROWS; row++) {
+            for (int column = 0; column < COLUMNS; column++) {
+                gridPosition.setPosition(column, row);
+                gridMap.get(gridPosition).setIsAlive(lifeGrid[column][row]);
+                grid.getChildren().add(gridMap.get(gridPosition));
             }
         }
-
-        // updates board with new cells
-        board.getChildren().clear();
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLUMNS; c++) {
-                cellPosition = c + " " + r;
-                board.getChildren().add(boardMap.get(cellPosition));
-            }
-        }
-
         generation++;
+        generationCounter.setText("generation: " + Integer.toString(generation));
+    }
 
-         // displayes a yellow generation counter
-        table.getChildren().remove(text);
-        text.setText(Integer.toString(generation));
-        table.getChildren().add(text);
+    Position checkPosition = new Position(0, 0);
+    Position aroundPosition = new Position(0, 0);
+
+    private int checkNeighboursAround(int column, int row) {
+
+        int neighbourColumn;
+        int neighbourRow;
+        int liveNeighbours = 0;
+
+        aroundPosition.clear();
+        aroundPosition.setPosition(column, row);
+
+        checkPosition.clear();
+        for (int columnBuffer = -1; columnBuffer < 2; columnBuffer++) {
+            for (int rowBuffer = -1; rowBuffer < 2; rowBuffer++) {
+                neighbourColumn = (column + columnBuffer + COLUMNS) % COLUMNS;
+                neighbourRow = (row + rowBuffer + ROWS) % ROWS;
+                checkPosition.setPosition(neighbourColumn, neighbourRow);
+                if (gridMap.get(checkPosition).getIsAlive() && !checkPosition.equals(aroundPosition)) {
+                    liveNeighbours++;
+                }
+            }
+        }
+        return liveNeighbours;
+    }
+
+    private void clearCells() {
+
+        generation = 0;
+        generationCounter.setText("generation: " + Integer.toString(generation));
+        Position cellPosition = new Position(0, 0);
+        grid.getChildren().clear();
+
+        for (int row = 0; row < ROWS; row++) {
+            for (int column = 0; column < COLUMNS; column++) {
+                cellPosition.setPosition(column, row);
+                gridMap.get(cellPosition).setIsAlive(false);
+                grid.getChildren().add(gridMap.get(cellPosition));
+            }
+        }
+    }
+
+    // randomly spawns living cells
+    private void nuke() {
+        boolean isAlive;
+        Position cellPosition = new Position(0, 0);
+
+        for (int row = 0; row < ROWS; row++) {
+            for (int column = 0; column < COLUMNS; column++) {
+                cellPosition.setPosition(column, row);
+                isAlive = ThreadLocalRandom.current().nextInt(0, 2) == 1;
+                gridMap.get(cellPosition).setIsAlive(isAlive);
+            }
+        }
+    }
+
+    private void initializeButtonListeners(Timeline timeline) {
+        grid.setOnDragDetected((MouseEvent t) -> {
+            grid.startFullDrag();
+        });
+
+        startButton.setOnAction((ActionEvent t) -> {
+            timeline.setCycleCount(Timeline.INDEFINITE);
+            timeline.play();
+        });
+
+        pauseButton.setOnAction((ActionEvent t) -> {
+            timeline.pause();
+        });
+
+        clearButton.setOnAction((ActionEvent t) -> {
+            timeline.pause();
+            clearCells();
+        });
+
+        nukeButton.setOnAction((ActionEvent t) -> {
+            nuke();
+        });
     }
 }
